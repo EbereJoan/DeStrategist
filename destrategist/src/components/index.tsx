498ff11c5b0ad96c3547c079afa5670267af85ee
EbereJoan <touch .gitignorewittybere@gmail.com>
2023-01-30 18:30:09 +0100

    debugging .tsx file

--- destrategist/src/components/index.tsx
+++ destrategist/src/components/index.tsx
@@ -4,52 +4,52 @@ import React from 'react';
 function App() {
   return (
     // <div classNameName={"App"} >
-    <div className = "hero">
+   <div className = "hero">
         <div className ="form-box">
             <div className="button-box">
                 <div id="btn">
-                    <button type="button" className="toggle-btn" onClick="login()">Log In</button>
-                    <button type="button" className="toggle-btn" onClick="register()">Register</button>
+                    {/* <button type="button" className="toggle-btn" onClick="login()" >Log In</button> */}
+                    {/* <button type="button" className="toggle-btn" onClick="register()">Register</button> */}
                 </div>
             </div>
-                <div className="social-icons">
-                <img src="facebook.jpg" />
-                <img src="twitter.jpg" />
-                <img src="insta.jpg" />
+            <div className="social-icons">
+                <img src="facebook.jpg" alt="" />
+                <img src="twitter.jpg" alt="" />
+                <img src="insta.jpg" alt="" />
             </div>
             <form id="login" className="input-group">
-                <input type="text" className="input-field" placeholder="User Id" required >
-                <input type="text" className="input-field" placeholder="Enter Password" required >
-                <input type="checkbox" className="chech-box" /> 
+                <input type="text" className="input-field" placeholder="User Id" required  alt="" />
+                <input type="text" className="input-field" placeholder="Enter Password" required  alt="" />
+                <input type="checkbox" className="chech-box" alt="" /> 
                 <span>Remember Password</span>
                 <button type="submit" className="submit-btn">Login</button>
             </form>  
-                <form id="register" className="input-group">
-                <input type="text" className="input-field" placeholder="User Id" required />
-                <input type="email" className="input-field" placeholder="Email Id" required />
-                <input type="text" className="input-field" placeholder="Enter Password" required />
-                <input type="text" className="input-field" placeholder="Confirm Password" required />
-                <input type="checkbox" className="chech-box" /> 
+            <form id="register" className="input-group">
+                <input type="text" className="input-field" placeholder="User Id" required alt="" />
+                <input type="email" className="input-field" placeholder="Email Id" required  alt=""/>
+                <input type="text" className="input-field" placeholder="Enter Password" required  alt="" />
+                <input type="text" className="input-field" placeholder="Confirm Password" required alt="" />
+                <input type="checkbox" className="chech-box" alt="" /> 
                 <span>I agree to the terms & conditions</span> 
                 <button type="submit" className="submit-btn">Register</button>
-                </form>  
-            </div>
+            </form>  
+
         </div>
-      <script>
+       <script>
             var x = document.getElementById("login");
             var y = document.getElementById("register");
             var z = document.getElementById("btn");
             
-            function register(){
-                x.style.left="-400px";
-                y.style.left="50px";
-                z.style.left="110px";
+            {/* function register(){
+                x.style.left="-400px",
+                y.style.left="50px",
+                z.style.left="110px",
             }
             function login(){
-                x.style.left="50px";
-                y.style.left="450px";
-                z.style.left="0px";
-            }
+                x.style.left="50px",
+                y.style.left="450px",
+                z.style.left="0px",
+            } */}
        
         </script>
     </div>
